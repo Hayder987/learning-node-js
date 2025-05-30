7fc5c74796b3fbab28f22814adbb7cce66d3e63e
hayder <hayderbd4290@gmail.com>
2025-05-30 19:19:53 +0600

    learning path

--- module-2/logger-app/index.js
+++ module-2/logger-app/index.js
@@ -3,12 +3,23 @@
 
 // console.log(process.argv)
 
+const fs = require("fs");
+
 const inputText = process.argv.slice(2);
-const text = inputText.join(' ');
+const message = inputText.join(' ')
+const time = new Date().toISOString();
+const text = `${message} At:${time} \n`;
 
 if(!text){
     console.log("example: node index.js hello world")
     process.exit(1)
 }
 
-console.log(text);
+const path = require('path')
+const pathName = path.join(__dirname, "log.txt")
+
+fs.appendFile(pathName, text, {encoding:"utf-8"}, ()=>{
+    console.log("log added successfully");
+})
+
+
